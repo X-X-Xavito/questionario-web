18b9c24910404d6276ecbe45d82a7ceaa97fa860
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-22 17:28:38 -0200

    Criação dos modelos: Questionario, Pergunta e Alternativa.
    Questionario recebe o atributo titulo
    Pergunta recebe os atribs. questionario(ForeignKey) e texto
    Alternativa recebe os atribs pergunta(ForeignKey), text e votos(integerfield)
    
    modelos já migrados no banco de dados

--- questionario/models.py
+++ questionario/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Questionario(models.Model):
+    titutlo = models.CharField(max_length=200)
+
+class Pergunta(models.Model):
+    questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+
+class Alternativa(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=200)
+    votos = models.IntegerField(default=0)
