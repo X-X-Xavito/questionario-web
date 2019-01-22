d6b4c6ea5c541e4abfba52d962a50866f63d06c4
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-22 17:54:47 -0200

    Adicionado o metodo __str__() para as classes Pergunta e Alternativa

--- questionario/models.py
+++ questionario/models.py
@@ -12,7 +12,13 @@ class Pergunta(models.Model):
     questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
     texto = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.texto
+
 class Alternativa(models.Model):
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     texto = models.CharField(max_length=200)
     votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.texto
