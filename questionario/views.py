2fb75389990516c5dfa2a46de8ab7eaa8825e0db
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-22 18:26:12 -0200

    Criação das views detail, results e vote.

--- questionario/views.py
+++ questionario/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Questionario, Pergunta, Alternativa
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questionarios = Questionario.objects.all()
+    context = {'questionarios': questionarios}
+    return render(request, 'questionario/index', context)
+
+def detail(request, questionario_id):
+    return HttpResponse("You're looking at question %s." % questionario_id)
+
+def results(request, questionario_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % questionario_id)
+
+def vote(request, questionario_id):
+    return HttpResponse("You're voting on question %s." % questionario_id)
