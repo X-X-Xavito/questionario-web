e233f0d1ccec2b835d92702fd96cab5ad04aa803
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-22 20:45:56 -0200

    Em questionario/admin.py criado as classes QuestionarioAdmin PerguntaAdmin e AlternativaAdmin e depois foram passadas como argumento em cada admin.site.register.
    Isso serve para facilitar a visualização dos itens no django-admin
    pelo admin do sistema

--- questionario/admin.py
+++ questionario/admin.py
@@ -3,8 +3,30 @@ from django.contrib import admin
 from .models import Questionario, Pergunta, Alternativa
 
 # Register your models here.
+class QuestionarioAdmin(admin.ModelAdmin):
+    fieldsets= [
+        (None, {'fields': ['titulo']}),
+    ]
 
-admin.site.register(Questionario)
-admin.site.register(Pergunta)
-admin.site.register(Alternativa)
+
+class PerguntaAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,             {'fields': ['texto']}),
+    ('Imagem da Foto', {'fields': ['imagem']}),        
+]
+    list_filter = ('questionario',)
+    list_display = ('texto','questionario',)
+
+
+class AlternativaAdmin(admin.ModelAdmin):
+    fieldsets = [
+    (None,             {'fields': ['texto']}),        
+]
+    list_filter = ('pergunta',)
+    list_display = ('texto','pergunta',)
+
+
+admin.site.register(Questionario, QuestionarioAdmin)
+admin.site.register(Pergunta, PerguntaAdmin)
+admin.site.register(Alternativa, AlternativaAdmin)
 
