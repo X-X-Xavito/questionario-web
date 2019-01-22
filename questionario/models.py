8d54313afd860411e2b81c2b7b95307f0e54ee5e
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-22 20:04:40 -0200

    Inclusao do atrb imagem(ImageField) à classe Pergunta e criação das pastas 'img' e 'static'
    Na pasta static está a imagem padrão para cada pergunta.
    A pasta img irá receber as imagens inseridas pelo admin do sistema

--- questionario/models.py
+++ questionario/models.py
@@ -11,6 +11,7 @@ class Questionario(models.Model):
 class Pergunta(models.Model):
     questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
     texto = models.CharField(max_length=200)
+    imagem = models.ImageField(default="python.jpg")
 
     def __str__(self):
         return self.texto
