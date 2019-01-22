1b724d971f519c336868c9ba96dcbd27927637da
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-22 18:09:09 -0200

    Adicionado as classes Questionario, Pergunta e Alternativa ao admin.py do app.
    Isso vai tornar essas classes visiveis quando acessar o admin do Django

--- questionario/admin.py
+++ questionario/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Questionario, Pergunta, Alternativa
+
 # Register your models here.
+
+admin.site.register(Questionario)
+admin.site.register(Pergunta)
+admin.site.register(Alternativa)
+
