ecca353c332c715eafeff352c1e7198acf74ac21
Kaique Xavier Atalla <kaiquexatalla@gmail.com>
2019-01-23 14:27:41 -0200

    Inserção de comentarios em models.py

--- questionario/models.py
+++ questionario/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+"""
+    Criação de três modelos no banco de dados. Cada modelo tem atributo.
+    Pergunta e Alternativa tem ForeignKey que define que esses modelos são atributos de outro modelo.
+    Metodo__str__ serve para exibir o objeto para o usuario
+    ImagemField serve para definir um atributo com imagem, sendo que a imagem padrão é 'python.jpg'
+"""
+
+
 class Questionario(models.Model):
     titulo = models.CharField(max_length=200)
     contador = models.IntegerField(default=0)
